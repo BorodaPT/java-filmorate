99690d73a8715c75a1a815312cf20755abcc0fb7
Boroda <via4eslavboroda@yandex.ru>
2023-05-19 13:21:19 +0300

    code style: исправлены

--- src/main/java/ru/yandex/practicum/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/controller/UserController.java
@@ -6,7 +6,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.model.Film;
 import ru.yandex.practicum.model.User;
 
 import javax.validation.Valid;
