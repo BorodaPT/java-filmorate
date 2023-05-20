8fca90af52653c504c1329e1d271163c490a92e2
Boroda <via4eslavboroda@yandex.ru>
2023-05-20 11:01:28 +0300

    code-style

--- src/main/java/ru/yandex/practicum/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/controller/FilmController.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.exception.DefaultMessageException;
 import ru.yandex.practicum.exception.ExceptionDataRequest;
 import ru.yandex.practicum.model.Film;
 
--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -1,21 +1,14 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import ru.yandex.practicum.model.User;
 
-import java.time.LocalDate;
 
 
 @SpringBootTest
