4ab2c927bf28d833d16888c3f75fd95194f42a5e
Boroda <via4eslavboroda@yandex.ru>
2023-05-22 19:23:09 +0300

    code-style

--- src/main/java/ru/yandex/practicum/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/storage/FilmStorage.java
@@ -6,12 +6,19 @@ import java.util.ArrayList;
 
 
 public interface FilmStorage {
+
     Film getFilm(long id);
+
     Film create(Film film);
+
     Film update(Film film);
+
     void delete(Long id);
+
     Film setLike(Long idFilm, Long idUser, boolean isInstalled);
+
     ArrayList<Film> getFilms();
+
     ArrayList<Film> getPopular(long count);
 
 }
--- src/main/java/ru/yandex/practicum/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/storage/UserStorage.java
@@ -6,12 +6,20 @@ import java.util.ArrayList;
 
 public interface UserStorage {
     User getUser(long id);
+
     User create(User user);
+
     User update(User user);
+
     void delete(Long id);
+
     ArrayList<User> getUsers();
+
     ArrayList<User> getFriend(long id);
+
     ArrayList<User> getCommonFriend(long idUserMain, long idUserFriend);
+
     User addFriendPost(long idUserMain, long idUserFriend);
+
     User deleteFriendPost(long idUserMain, long idUserFriend);
 }
